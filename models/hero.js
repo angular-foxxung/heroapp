5c8a1376459e7c29fcd074d31f8d7844ba595ba3
Andreas Streichardt <andreas.streichardt@gmail.com>
2014-04-27 23:49:30 +0200

    Task 1

--- models/hero.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    "use strict";
-
-    var Foxx = require("org/arangodb/foxx");
-    var Hero = Foxx.Model.extend({}, {"attributes":{"name": "string", "comment": "string", "_key": "string", "_id": "string"}});
-
-    exports.Model = Hero;
-})();
--- repositories/heroes.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    "use strict";
-
-    var Foxx = require("org/arangodb/foxx");
-    var Heroes = Foxx.Repository.extend({});
-
-    exports.Repository = Heroes;
-})();
